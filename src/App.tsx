1df304b98d4a9d39c8fcb1787c43252305925851
keisuke6065 <keisuke6065@gmail.com>
2021-11-11 17:00:01 +0900

    add problem text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import {Fragment} from 'react'
+import {useEffect, useState} from 'react'
 
 
-export const App = () => (<Fragment>
-  <h1>sample</h1>
-</Fragment>)
+export const App = () => {
+  // create count state default 0
+  // example const [state, setState] = useState(default value)
+  // create text state default ''
 
+  // create functions
+  // countUp count + 1
+  // countDown count -1
+  // cleanUp 0 reset
+
+  // create side effects functions
+  // example useEffect(() => {/*side effect*/}, /*dependency List*/)
+  // count = 3multiple is text = 3multiple
+  // count != 3multiple is text = count: {count}
+
+  return (<>
+    <h1>cont: 0 or 3multiple</h1>
+    <button>count up</button>
+    <button>count down</button>
+    <button>count clean</button>
+  </>)
+}
