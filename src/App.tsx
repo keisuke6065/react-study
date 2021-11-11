7c96cb9aa98f8cd2fbcc1713acad4b8460faf7a0
keisuke6065 <keisuke6065@gmail.com>
2021-11-11 17:05:41 +0900

    add answer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import {useEffect, useState} from 'react'
 
-
 export const App = () => {
-  // create count state default 0
-  // example const [state, setState] = useState(default value)
-  // create text state default ''
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
 
-  // create functions
-  // countUp count + 1
-  // countDown count -1
-  // cleanUp 0 reset
-
-  // create side effects functions
-  // example useEffect(() => {/*side effect*/}, /*dependency List*/)
-  // count = 3multiple is text = 3multiple
-  // count != 3multiple is text = count: {count}
+  const countUp = () => setCount((prevState) => prevState + 1)
+  const countDown = () => setCount((prevState) => prevState - 1)
+  const cleanUp = () => setCount(() => 0)
+  useEffect(() => {
+    setText(count % 3 === 0 && count !== 0 ? '3multiple': `click count ${count}`)
+  }, [count])
 
   return (<>
-    <h1>cont: 0 or 3multiple</h1>
-    <button>count up</button>
-    <button>count down</button>
-    <button>count clean</button>
+    <h1>{text}</h1>
+    <h2>count: {count}</h2>
+    <button onClick={countUp}>up</button>
+    <button onClick={countDown}>down</button>
+    <button onClick={cleanUp}>clean</button>
   </>)
 }
