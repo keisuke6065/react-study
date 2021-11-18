d0caf12bb988cf230bfa5376982662bdebec2c3b
keisuke6065 <keisuke6065@gmail.com>
2021-11-18 21:37:01 +0900

    add chapter5 answer

--- src/hooks/useCounter.test.ts
+++ src/hooks/useCounter.test.ts
@@ -8,25 +8,32 @@ describe('useCounter', () => {
     result = renderHook(() => useCounter()).result
   })
 
-  test('default count 0', () => {
-    // count get variable = result.current.count
+  test('default count', () => {
+    expect(result.current.count).toBe(0)
   })
   test('count up', () => {
+    expect(result.current.count).toBe(0)
     act(() => {
       result.current.countUp()
     })
+    expect(result.current.count).toBe(1)
   })
   test('count down', () => {
+    expect(result.current.count).toBe(0)
     act(() => {
       result.current.countDown()
     })
+    expect(result.current.count).toBe(-1)
   })
   test('count clean up', () => {
+    expect(result.current.count).toBe(0)
     act(() => {
       result.current.countUp()
     })
+    expect(result.current.count).toBe(1)
     act(() => {
       result.current.cleanUp()
     })
+    expect(result.current.count).toBe(0)
   })
 })
